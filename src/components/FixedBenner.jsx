2e4c679254b18a9f419fc6a81728b1cf2e297784
shahinexy <shahinexy@gmail.com>
2024-05-19 12:15:09 +0600

    our menu page added

--- src/components/FixedBenner.jsx
+++ src/components/FixedBenner.jsx
@@ -1,9 +1,9 @@
 import benner from '../assets/home/chef-service.jpg'
 
-const FixedBenner = ({title, des, img}) => {
+const FixedBenner = ({title, des, img, bg, height, text}) => {
     return (
-        <div style={{backgroundImage:` url(${img})`}} className='bg-fixed bg-cover bg-center md:p-24 p-7'>
-        <div className=' text-black bg-white/70 text-center md:px-32 md:py-24 p-7'>
+        <div style={{backgroundImage:` url(${img})`}} className={`bg-fixed bg-cover flex justify-center items-center bg-center md:p-24 p-7 ${height} ${text || "text-black"}`}>
+        <div className={` text-center md:px-32 md:py-24 p-7 ${bg || "bg-white/70"}`}>
             <h2 className='md:text-4xl text-3xl mb-6 uppercase'>{title}</h2>
             <p>{des}</p>
         </div>
--- src/pages/OurMenu/OurMenu.jsx
+++ src/pages/OurMenu/OurMenu.jsx
@@ -1,10 +1,76 @@
+import FixedBenner from "../../components/FixedBenner";
+import benner from "../../assets/menu/banner3.jpg";
+import useMenu from "../../hooks/useMenu";
+import SectionTitle from "../../components/SectionTitle";
+import SectionMenu from "../../components/SectionMenu";
+import dessertBenner from "../../assets/menu/dessert-bg.jpeg";
+import pizzaBenner from "../../assets/menu/pizza-bg.jpg";
 
 const OurMenu = () => {
-    return (
-        <div>
-            
+  const { menuItems } = useMenu();
+  const offered = menuItems.filter((item) => item.category === "offered");
+  const dessert = menuItems.filter((item) => item.category === "dessert");
+  const pizza = menuItems.filter((item) => item.category === "pizza");
+  const salad = menuItems.filter((item) => item.category === "salad");
+  const soup = menuItems.filter((item) => item.category === "soup");
+  return (
+    <div>
+      <FixedBenner
+        img={benner}
+        title={"our menu"}
+        des={"Would you like to try a dish?"}
+        bg={"bg-black/50"}
+        height={"h-screen"}
+        text={"text-white"}
+      ></FixedBenner>
+
+      <div className="max-w-7xl mx-auto my-24">
+        <SectionTitle
+          subTitle={"Don't miss"}
+          title={"Today's offer"}
+        ></SectionTitle>
+
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-10">
+          {offered.map((data) => (
+            <SectionMenu key={data._id} data={data}></SectionMenu>
+          ))}
         </div>
-    );
+
+        <div className="my-24">
+          <FixedBenner
+            title={"dessert"}
+            des={
+              "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            }
+            img={dessertBenner}
+          ></FixedBenner>
+        </div>
+
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-10">
+          {dessert.map((data) => (
+            <SectionMenu key={data._id} data={data} img></SectionMenu>
+          ))}
+        </div>
+
+
+        <div className="my-24">
+          <FixedBenner
+            title={"pizza"}
+            des={
+              "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            }
+            img={pizzaBenner}
+          ></FixedBenner>
+        </div>
+
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-10">
+          {pizza.map((data) => (
+            <SectionMenu key={data._id} data={data} img></SectionMenu>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OurMenu;
