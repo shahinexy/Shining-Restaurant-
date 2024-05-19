ee305b2626d2004e603f92a4b04b5e50c6f96fc6
shahinexy <shahinexy@gmail.com>
2024-05-19 10:18:54 +0600

    get review data

--- src/pages/Home/Testimonial.jsx
+++ src/pages/Home/Testimonial.jsx
@@ -7,6 +7,7 @@ import "swiper/css/navigation";
 
 // import required modules
 import { Navigation } from "swiper/modules";
+import { FaQuoteLeft } from "react-icons/fa";
 
 const Testimonial = () => {
   const [reviews, setReviews] = useState([]);
@@ -17,10 +18,16 @@ const Testimonial = () => {
   }, []);
 
   return (
-    <div>
+    <div className="max-w-4xl mx-auto">
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         {
-            reviews.map(data => <SwiperSlide key={data._id}>Slide 1</SwiperSlide>)
+            reviews.map(data => <SwiperSlide key={data._id}>
+                    <div className="sm:px-28 p-10 ">
+                        <div className="flex justify-center mb-6"><FaQuoteLeft className="text-5xl " /></div>
+                        <p className="text-center">{data.details}</p>
+                        <p className="text-xl font-semibold text-center text-orange-500 mt-4 uppercase">{data.name}</p>
+                    </div>
+                </SwiperSlide>)
         }
       </Swiper>
     </div>
