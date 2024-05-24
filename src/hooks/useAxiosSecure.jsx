864eddca7db70f903e0a61f0aa5fe285d566082c
shahinexy <shahinexy@gmail.com>
2024-05-24 23:57:59 +0600

    used interceptor in AxiosSecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,22 @@
 import axios from "axios";
 
 export const axisoSecure = axios.create({
-    baseURL: 'http://localhost:5000'
-})
+  baseURL: "http://localhost:5000",
+});
 const useAxiosSecure = () => {
-    return axisoSecure;
+    axisoSecure.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('access-token')
+        console.log('request stop by interceptor',token);
+        config.headers.authoraization = `Bearer ${localStorage.getItem('access-token')}`
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  return axisoSecure;
 };
 
 export default useAxiosSecure;
