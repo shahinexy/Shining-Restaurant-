3c45a73daedbe116f8dc338b68cd526956c423ee
shahinexy <shahinexy@gmail.com>
2024-05-25 09:28:50 +0600

    admin route created

---
+++ src/router/AdminRoute.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { authContext } from "../AuthProvider/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
+import { Spinner } from "@nextui-org/react";
+
+const AdminRoute = ({children}) => {
+    const {user, loading} = useContext(authContext)
+    const location = useLocation()
+    const [isAdmin, isAdminLoading] = useAdmin()
+
+    if(loading || isAdminLoading){
+        return <div className="w-full h-screen flex justify-center items-center"> <Spinner label="Warning" color="warning" labelColor="warning"/></div>
+    }
+
+    if(user && isAdmin) {
+        return children;
+    }
+    return <Navigate to={'/login'} state={{from: location}} replace></Navigate>;
+};
+
+export default AdminRoute;
