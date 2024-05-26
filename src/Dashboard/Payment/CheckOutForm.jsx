4dc8d90208dbb832ae3f02b7f85f6f52c466fb11
shahinexy <shahinexy@gmail.com>
2024-05-26 23:46:08 +0600

    stripe payment method added

---
+++ src/Dashboard/Payment/CheckOutForm.jsx
@@ -0,0 +1,64 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+
+const CheckOutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card === null) {
+      return;
+    }
+
+    // Use your card Element with other Stripe.js APIs
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("[error]", error);
+    } else {
+      console.log("[PaymentMethod]", paymentMethod);
+    }
+
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="w-1/2 mx-auto">
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          type="submit"
+          disabled={!stripe}
+          className="bg-orange-500 px-5 py-2 my-5 text-white text-xl font-semibold"
+        >
+          Pay
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CheckOutForm;
--- src/Dashboard/Payment/Payment.jsx
+++ src/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,20 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../components/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "./CheckOutForm";
+
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GETWAY_PK);
 
 const Payment = () => {
     return (
         <div>
             <SectionTitle title={'Payment'}></SectionTitle>
+            
+            <div>
+                <Elements stripe={stripePromise}>
+                    <CheckOutForm></CheckOutForm>
+                </Elements>
+            </div>
         </div>
     );
 };
